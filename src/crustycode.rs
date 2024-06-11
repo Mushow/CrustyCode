0ded64bfd2e7e32097d5646f62e45780c8edb3b5
Mushow <mushow@proton.me>
2024-06-12 00:01:41 +0200

    Implemented read_binary_file function

--- src/crustycode.rs
+++ src/crustycode.rs
@@ -1,3 +1,27 @@
+use std::{fs, io};
+use std::io::Read;
+
 pub fn run(file_path: &str) {
     println!("Analyzing file: {}", file_path);
+
+    match fs::metadata(file_path) {
+        Ok(_) => match read_binary_file(file_path) {
+            Ok(data) => {
+                println!("Read {} bytes from file.", data.len());
+            },
+            Err(e) => {
+                eprintln!("Error reading file: {}", e);
+            }
+        },
+        Err(e) => {
+            eprintln!("File does not exist or cannot be accessed: {}", e);
+        }
+    }
+}
+
+fn read_binary_file(path: &str) -> io::Result<Vec<u8>> {
+    let mut file = fs::File::open(path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
 }
